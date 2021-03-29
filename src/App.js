ddaf6ce10be0e8a4a62be5236ac12980ab8d8c4b
Milos Subasic <msubasic@itekako.com>
2021-03-29 14:35:31 +0200

    first run

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import './app.scss';
+import images from './data/home';
 
 const App = () => {
-  return ( <p>Proba</p> );
+  return ( 
+    <div>
+      <h1>Welcome to the dreamsApp!</h1>
+      <h3>Dreams can tell us A LOT about ourselves - as famously Froyd once put it: ''Dreams are a royal highway to unconscious''. They can guide us, they can warn us - predict,</h3>
+      <section className='container'>
+          <div className='container__images'>
+              {images.map(image => {
+                return <img src={image.photo && image.photo.file.url} alt={image.altText}/>
+              })}
+          </div>
+      </section>
+      <div>
+          Features section.
+          <ul>
+              <li>ovo</li>
+              <li>ono</li>
+              <li>ovo</li>
+              <li>ono</li>
+          </ul>
+      </div>
+    </div>
+    );
 }
  
 export default App;
---
+++ src/data/home.js
@@ -0,0 +1,48 @@
+import brainWaves from '../images/home/brain-waves.png';
+
+const images = [
+    {
+        id: 'i1',
+        photo: brainWaves,
+        link: 'https://naturallakeland.com/all-about-brainwaves/',
+        altText: 'brain-waves'
+    },
+    {
+        id: 'i2',
+        photo: brainWaves,
+        link: 'https://en.wikipedia.org/wiki/Carl_Jung',
+        altText: 'Carl Jung'
+    },
+    {
+        id: 'i3',
+        photo: brainWaves,
+        link: '#',
+        altText: 'cow'
+    },
+    {
+        id: 'i4',
+        photo: brainWaves,
+        link: 'https://www.amazon.com/Tibetan-Yogas-Dream-Sleep/dp/1559391014/ref=sr_1_1?crid=KTMFAQUARF1N&dchild=1&qid=1616426581',
+        altText: 'Dream Yoga'
+    },
+    {
+        id: 'i5',
+        photo: brainWaves,
+        link: 'https://tothestars.media/blogs/news/the-science-behind-lucid-dreaming',
+        altText: 'lucid dreaming'
+    },
+    {
+        id: 'i6',
+        photo: brainWaves,
+        link: 'https://en.wikipedia.org/wiki/Sigmund_Freud',
+        altText: 'Sigmund Freud'
+    },
+    {
+        id: 'i7',
+        photo: brainWaves,
+        link: 'http://ivannastovic.com/-Biografije/Biografija-Ivan-Nastovic/',
+        altText: 'Ivan Nastovic'
+    }
+];
+
+export default images;
