c3690e436282308b51c1c452ffd0280e12223802
Milos Subasic <msubasic@itekako.com>
2021-04-09 18:02:39 +0200

    some minor changes

--- src/components/list-of-dreams/dream.js
+++ src/components/list-of-dreams/dream.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Dream = ({ dreamData }) => {
-    console.log(dreamData);
     const { content, fullDate, title } = dreamData;
     return (
         <div>
--- src/pages/list-of-dreams.js
+++ src/pages/list-of-dreams.js
@@ -7,32 +7,27 @@ import Dream from '../components/list-of-dreams/dream';
 class ListOfDreams extends Component {
     state = { 
         listOfDreams: null,
-        loading: false
+        loading: false,
+        error: null
      };
 
     componentDidMount() {
         this.setState({loading: true});
-        fetchDreams()
+        fetchDreams.get()
         .then ( response => {
-            console.log(response);
-            console.log(response.data);
             const dreamsArray = repackingFetchedDreams(response.data);
-            console.log(dreamsArray);
             this.setState({
                 listOfDreams: dreamsArray,
                 loading: false
-            });
-            console.log(this.state.listOfDreams);
+            })
         })
+        .catch (  error => console.log(error))
     };
 
     render() {
         const conditionalDreamsRendering = this.state.listOfDreams && this.state.listOfDreams.length > 0 ?
                                            this.state.listOfDreams.map(dream => <Dream key={dream.id} dreamData={dream}/>) :
                                            <div>Loading baby...</div>;   
-        // const conditionalDreamsRendering = this.state.listOfDreams ?
-        //                                    <div>Patak Daca, barem nesto radi</div> :
-        //                                    <div>Loading baby...</div>;   
         return (
             <>
                 <h3>These are your dreams mate</h3>
