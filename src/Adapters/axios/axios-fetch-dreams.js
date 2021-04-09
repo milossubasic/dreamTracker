a707fb11609ed000daebfda77f4469a129a18583
Milos Subasic <msubasic@itekako.com>
2021-04-09 15:38:10 +0200

    basically only important thing hear is editing axios baseURL

--- src/Adapters/axios/axios-fetch-dreams.js
+++ src/Adapters/axios/axios-fetch-dreams.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const fetchDreams = axios.create({
-    baseURL: 'https://react-dreamstracker-default-rtdb.firebaseio.com/'
+    baseURL: 'https://react-dreamstracker-default-rtdb.firebaseio.com/dreams.json'
 });
 
 export default fetchDreams;
