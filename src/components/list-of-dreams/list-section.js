b00efeedf1e382acf84025642ede9fcd911e572c
Milos Subasic <msubasic@itekako.com>
2021-04-10 12:06:22 +0200

    migrating most of the list-of-dreams code to list-section component

---
+++ src/components/list-of-dreams/list-section.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import fetchDreams from '../../Adapters/axios/axios-fetch-dreams';
+import repackingFetchedDreams from '../../helpers/repacking-fetched-dreams';
+
+import Dream from './dream';
+
+class ListSection extends Component {
+    state = { 
+        listOfDreams: null,
+        loading: false,
+        error: null
+     };
+
+    componentDidMount() {
+        this.setState({loading: true});
+        fetchDreams.get()
+        .then ( response => {
+            const dreamsArray = repackingFetchedDreams(response.data);
+            this.setState({
+                listOfDreams: dreamsArray,
+                loading: false
+            })
+        })
+        .catch (  error => console.log(error))
+    };
+
+    render() {
+        const conditionalDreamsRendering = this.state.listOfDreams && this.state.listOfDreams.length > 0 ?
+                                           this.state.listOfDreams.map(dream => <Dream key={dream.id} dreamData={dream}/>) :
+                                           <div>Loading baby...</div>;   
+        return (
+            <div className='container'>
+                {conditionalDreamsRendering}
+            </div> 
+         );
+    };
+};
+ 
+export default ListSection;
--- src/pages/list-of-dreams.js
+++ src/pages/list-of-dreams.js
@@ -1,43 +1,18 @@
-import React, { Component } from 'react';
-import fetchDreams from '../Adapters/axios/axios-fetch-dreams';
-import repackingFetchedDreams from '../helpers/repacking-fetched-dreams';
+import React from 'react';
 
 import Heading from '../components/layout/heading/heading';
-import Dream from '../components/list-of-dreams/dream';
+import ListSection from '../components/list-of-dreams/list-section';
 
 import dataListOfDreams from '../data/data-list-of-dreams';
 
-class ListOfDreams extends Component {
-    state = { 
-        listOfDreams: null,
-        loading: false,
-        error: null
-     };
+const ListOfDreams = () => {
 
-    componentDidMount() {
-        this.setState({loading: true});
-        fetchDreams.get()
-        .then ( response => {
-            const dreamsArray = repackingFetchedDreams(response.data);
-            this.setState({
-                listOfDreams: dreamsArray,
-                loading: false
-            })
-        })
-        .catch (  error => console.log(error))
-    };
-
-    render() {
-        const conditionalDreamsRendering = this.state.listOfDreams && this.state.listOfDreams.length > 0 ?
-                                           this.state.listOfDreams.map(dream => <Dream key={dream.id} dreamData={dream}/>) :
-                                           <div>Loading baby...</div>;   
-        return (
-            <>
-                <Heading content={dataListOfDreams.headContent} />
-                {conditionalDreamsRendering}
-            </> 
-         );
-    };
+    return (
+        <>
+            <Heading content={dataListOfDreams.headContent} />
+            <ListSection />
+        </>
+    )
 };
  
 export default ListOfDreams;
