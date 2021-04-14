796d53c31e2fc6eb1215b8440eaea79f4a2b7680
Milos Subasic <msubasic@itekako.com>
2021-04-14 16:19:54 +0200

    list-section component is connected to redux

--- src/components/list-of-dreams/list-section.js
+++ src/components/list-of-dreams/list-section.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import fetchDreams from '../../Adapters/axios/axios-fetch-dreams';
-import repackingFetchedDreams from '../../helpers/repacking-fetched-dreams';
+// import fetchDreams from '../../Adapters/axios/axios-fetch-dreams';
+// import repackingFetchedDreams from '../../helpers/repacking-fetched-dreams';
+import * as actionCreator from '../../store/actions/actionCreators';
+import { connect } from 'react-redux';
 
 import './list-section.scss';
 
@@ -14,22 +16,26 @@ class ListSection extends Component {
      };
 
     componentDidMount() {
-        this.setState({loading: true});
-        fetchDreams.get()
-        .then ( response => {
-            const dreamsArray = repackingFetchedDreams(response.data);
-            this.setState({
-                listOfDreams: dreamsArray,
-                loading: false
-            })
-        })
-        .catch (  error => console.log(error))
+        this.props.onDidMount();
+        // this.setState({loading: true});
+        // fetchDreams.get()
+        // .then ( response => {
+        //     const dreamsArray = repackingFetchedDreams(response.data);
+        //     this.setState({
+        //         listOfDreams: dreamsArray,
+        //         loading: false
+        //     })
+        // })
+        // .catch (  error => console.log(error))
     };
 
     render() {
-        const conditionalDreamsRendering = this.state.listOfDreams && this.state.listOfDreams.length > 0 ?
-                                           this.state.listOfDreams.map(dream => <li key={dream.id}><Dream dreamData={dream} /></li>) :
-                                           <div>Loading baby...</div>;   
+        const conditionalDreamsRendering = this.props.loading ? <p>Loading...</p> :
+        this.props.error ? <p>{this.props.error}</p> :
+            this.props.dreams && this.props.dreams.length > 0 ?
+               this.state.listOfDreams.map(dream => <li key={dream.id}><Dream dreamData={dream} /></li>) :
+               <div>No Dreams to show</div>;   
+
         return (
             <section className='list-section'>
                 <div className='container'>
@@ -41,5 +47,19 @@ class ListSection extends Component {
          );
     };
 };
+
+const mapStateToProps = state => {
+    return {
+        dreams: state.dreams,
+        loading: state.loading,
+        error: state.error
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onDidMount: () => dispatch(actionCreator.dreams_fetching())
+    };
+};
  
-export default ListSection;
+export default connect(mapStateToProps, mapDispatchToProps)(ListSection);
