7642a0cfc79e0e8f268cc924ba04bd966936a8fd
Milos Subasic <msubasic@itekako.com>
2021-04-14 15:03:46 +0200

    redux, react-redux, redux devtools and thunk are installed, index.js is set

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, applyMiddleware, compose } from 'react-router-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './store/reducers/dreamsReducer';
+import thunk from 'redux-thunk'
+
 import App from './App';
-//import Index from './pages/index';
-import { BrowserRouter } from 'react-router-dom';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
     document.getElementById('root')
 );
