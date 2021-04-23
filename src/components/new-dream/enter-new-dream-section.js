899cb0573f5dd1316d6c14229ab33e0bf1350a04
Milos Subasic <msubasic@itekako.com>
2021-04-23 15:12:28 +0200

    there is now dummy new-dream page, and it is hooked up with router

---
+++ src/components/new-dream/enter-new-dream-section.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EnterNewDreamSection = () => {
+    return ( <div>Ovde ce biti silni input</div> );
+}
+ 
+export default EnterNewDreamSection;
---
+++ src/pages/new-dream.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import EnterNewDreamSection from '../components/new-dream/enter-new-dream-section';
+
+const NewDream = () => {
+    return ( 
+        <EnterNewDreamSection/>
+     );
+}
+ 
+export default NewDream;
