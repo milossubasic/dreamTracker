366c6b1c7e1ac8c21e48bc5da3818770f9ee4d89
Milos Subasic <msubasic@itekako.com>
2021-04-26 10:25:01 +0200

    there is now data file for this page, and it is connected with the component

---
+++ src/data/data-new-dream.js
@@ -0,0 +1,14 @@
+
+const newDream = {
+    headContent: {
+        subTitle: 'Hooray, you had a new downfall!'
+    },
+    selectElements: [
+        {
+            id: 'ndse1',
+            numberOfDays: 31,
+        }
+    ]
+};
+
+export default newDream;
--- src/pages/new-dream.js
+++ src/pages/new-dream.js
@@ -3,10 +3,13 @@ import React from 'react';
 import Heading from '../components/layout/heading/heading'
 import EnterNewDreamSection from '../components/new-dream/enter-new-dream-section';
 
+import dataNewDream from '../data/data-new-dream';
+
 const NewDream = () => {
+    const { headContent } = dataNewDream;
     return (
         <> 
-            <Heading content={{subTitle: 'Hooray, you had a new dream!'}}/>
+            <Heading content={headContent}/>
             <EnterNewDreamSection/>
         </>
      );
