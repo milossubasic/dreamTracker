1748a95315fd6b7aa1ed6cc101c83ff4f0a4df7b
Milos Subasic <msubasic@itekako.com>
2021-04-26 09:44:59 +0200

    heading is added

--- src/pages/new-dream.js
+++ src/pages/new-dream.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Heading from '../components/layout/heading/heading'
 import EnterNewDreamSection from '../components/new-dream/enter-new-dream-section';
 
 const NewDream = () => {
-    return ( 
-        <EnterNewDreamSection/>
+    return (
+        <> 
+            <Heading content={{subTitle: 'Hooray, you had a new dream!'}}/>
+            <EnterNewDreamSection/>
+        </>
      );
 }
  
