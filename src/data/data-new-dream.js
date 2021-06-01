3dfa5529e3b455f9faf4f409cce1637483e2e5a8
Milos Subasic <msubasic@itekako.com>
2021-06-01 11:30:28 +0200

    more details to styling and adding hint for date

--- src/data/data-new-dream.js
+++ src/data/data-new-dream.js
@@ -8,20 +8,21 @@ const newDream = {
             inputType: 'date',
             id: 'date',
             labelText: 'Date',
+            hintText: 'When the dream happened'
         },
         {
             inputType: 'text',
             id: 'title',
             labelText: 'Title',
             placeholderText: 'Night out',
-            hintText: 'This title will be displayed in detailed view of the dream'
+            hintText: 'Will be displayed in detailed view of a dream'
         },
         {
             inputType: 'textArea',
             id: 'summary',
             labelText: 'Summary',
             placeholderText: 'I went out with Ana and had wonderful time, untill Dracula showed up. ',
-            hintText: 'This summery will be displayed in list view of your dreams'
+            hintText: 'Will be displayed in list view of your dreams'
         },
         {
             inputType: 'textArea',
