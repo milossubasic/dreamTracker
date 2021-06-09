d91f1ef83a215daa5c52d82df70b3861eae7c6a3
Milos Subasic <msubasic@itekako.com>
2021-06-09 13:09:54 +0200

    and the styling has begon!

--- src/components/dream/dream-section.js
+++ src/components/dream/dream-section.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import dreamPropType from '../../helpers/dream-prop-type';
 
+import './dream-section.scss';
+
 import DreamNavigation from './dream-navigation/dream-navigation';
 
 const Dream = ({ theDream }) => {
     const {story, date, title} = theDream;
     return ( 
-        <section>
-            <div>
-                <div>{date.date}, {date.day}</div>
-                <div>{title}</div>
-                <div>{story}</div>
+        <section className='dream-section'>
+            <div className='container'>
+                <div>
+                    <div className='dream-section__date'>{date.day}, {date.date}.{date.month}.</div>
+                    <h4 className='dream-section__title'>{title}</h4>
+                    <div className='dream-section__story'>{story}</div>
+                </div>
+                <DreamNavigation/>
             </div>
-            <DreamNavigation/>
         </section>
      );
 };
