f4b7f51e3da2373e795cf54e9256c510914010f8
Milos Subasic <msubasic@itekako.com>
2021-07-15 15:56:23 +0200

    added text and some styling

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './footer.scss';
 
 const Footer = () => {
-    return ( <div> Ovo ti je futera!? </div> );
+    return (
+        <footer>
+            <div className='container centering'>
+                <div className='footer'>
+                    <div className='footer__noLove'> Made without  <span className='footer__heart'></span>  by Zulumćar i Mostograditelj</div>
+                    <div> No copy rights of whatever kind</div>
+                    <div>&times;</div>
+                    <div> Go ahaed and fuck the world up</div>
+                </div>
+            </div>
+        </footer>
+    );
 }
  
 export default Footer;
