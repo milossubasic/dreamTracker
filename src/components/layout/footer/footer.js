126b98667700a3f6549fe0f59a8df1684046c18e
Milos Subasic <msubasic@itekako.com>
2021-07-21 18:30:18 +0200

    added some styling

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -8,7 +8,6 @@ const Footer = () => {
                 <div className='footer'>
                     <div className='footer__noLove'> Made without  <span className='footer__heart'></span>  by Zulumćar i Mostograditelj</div>
                     <div> No copy rights of whatever kind</div>
-                    <div>&times;</div>
                     <div> Go ahaed and fuck the world up</div>
                 </div>
             </div>
